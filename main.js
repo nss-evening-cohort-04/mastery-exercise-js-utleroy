80ac7768e05855ab1db713449ed7753dcb18d2da
Bryan Hankins <bryanhankins@me.com>
2016-08-01 23:16:13 -0500

    numbers iterating

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
-var userNum = document.getElementById("height");
-var userChar = document.getElementById("character");
 var button = document.getElementById("grow")
-// var treeSize = {
-//     number: userNum,
-//     character: userChar
-// };
-button.addEventListener("click", growTree)
+var treeObj = {};
 
+button.addEventListener("click", userInputForTree)
 
+/*this function is used to get the initial user input to display in the console*/
+function userInputForTree() {
+  treeObj.height = document.getElementById("height").value;
+  treeObj.character = document.getElementById("character").value;
+  var showUserInputInConsole = treeObj.height + treeObj.character
+  console.log("user input:", showUserInputInConsole)
+  treeDisplay()
+}
 
-
-
-function growTree() {
-  var display = userNum.value + userChar.value
-  console.log(display)
+function treeDisplay() {
+  var waterIt = "";
+  var incrementRows = (2*treeObj.height) - 1;
+  for (var i = 0; i < treeObj.height; i++) {
+    var wide = (i * 2) + 1;
+    // waterIt;
+    console.log(wide)
+  }
 }
