fade21d11354232a6a39cf05831af3a2ea08e393
Bryan Hankins <bryanhankins@me.com>
2016-08-10 09:08:14 -0500

    final push for quiz review

--- main.js
+++ main.js
@@ -1,51 +1,52 @@
+/*variables for input fields and the grow tree button*/
+
 var button = document.getElementById("grow")
 var heightVal = document.getElementById("height")
 var characterVal = document.getElementById("character")
 
+/*event lsitener to detect when the grow tree button is pushed*/
 button.addEventListener("click", userInputForTree)
 
+/*this function is used to get the initial user input.*/
+function userInputForTree() {
+  var rows = heightVal.value;
+  var width = characterVal.value;
+  checkForProperFormat(rows, width);
+}
+
 /*checking that each input field has a value*/
-function checkForProperFormat() {
-  if(!heightVal || !characterVal) {
+function checkForProperFormat(rows, width) {
+  if(!rows || !width) {
     alert("Both input fields need a value!");
   } else {
-    treeDisplay();
+    buildTree(rows, width);
   }
 }
 
-/*this function is used to get the initial user input to display in the console*/
-function userInputForTree() {
-  heightVal = document.getElementById("height").value;
-  characterVal = document.getElementById("character").value;
-  checkForProperFormat();
-}
-
-/*this is creating the empty tree object and the waterTree empty string*/
-var treeObj = {};
-var waterTree = "";
-
-/*function for building the tree from the user input.*/
+/*function for building the tree from the user input. Then calls the treeDisplay function*/
 function buildTree(rows, width) {
-  var rows = (2*heightVal) - 1;
-  for (var i = 0; i < heightVal; i++) {
-    var width = (i * 2) + 1;
-    for (var k = 0; k < width; k++) {
-      waterTree += characterVal
-    }
-    console.log(waterTree)
+var treeObj = {
+  char: width,
+  tall: rows
+};
+  treeDisplay(treeObj);
+}
+
+/*function for displaying the tree after it runs through the buildTree function*/
+function treeDisplay(tree) {
+  var h = tree.tall;
+  for (var i = 0; i < tree.tall; i++) {
+    var whitespace = (h - 1) - i;
+    var num = 2 * i + 1;
+
+    console.log(" ".repeat(whitespace) + tree.char.repeat(num));
   }
 }
 
-/*function for displaying the tree after it runs through the buildTree funciton*/
-function treeDisplay(rows, width) {
-  var whitespace = (rows - width) / 2;
-  for (var j = 0; j < whitespace; j++) {
-    waterTree += " &nbsp;"
-  }
-  buildTree()
-}
-
+/*event listener for the function enterKeyPressed()*/
 document.addEventListener("keyPress", treeDisplay);
+
+/*function to detect keypress when the enter||return key is pressed. Then executes the function treeDisplay()*/
 
 function enterKeyPressed (keyPress) {
   if (keyPress.which === 13) {
